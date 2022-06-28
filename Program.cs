a9dae200844747fdd1c4f8713f078e0ebbe7e88d
Ridvan Ermaya <ridvanermaya@gmail.com>
2022-06-27 20:00:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tiger_pistol_code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deckOfCards = new DeckOfCards();
+
+            var deck = deckOfCards.CreateDeck();
+
+            deck = deckOfCards.Shuffle(deck);
+
+            deckOfCards.DealAll(deck);
+        }
+    }
+}
---
+++ tiger-pistol-code-challenge/models/Card.cs
@@ -0,0 +1,9 @@
+namespace tiger_pistol_code_challenge.models
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+
+        public string Value { get; set; }
+    }
+}
