aa6d579d885021f986a1f5e0259240521ca29740
Ridvan Ermaya <ridvanermaya@gmail.com>
2022-06-28 22:10:29 -0500

    restructred the project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tiger_pistol_code_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deck = new Deck();
+        }
+    }
+}
--- tiger-pistol-code-challenge/models/Card.cs
+++
@@ -1,9 +0,0 @@
-namespace tiger_pistol_code_challenge.models
-{
-    public class Card
-    {
-        public string Suit { get; set; }
-
-        public string Value { get; set; }
-    }
-}
